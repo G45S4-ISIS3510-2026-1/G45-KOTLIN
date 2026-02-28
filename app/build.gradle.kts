5ff4e362b89eb201d637c1ab96b5f66bd99b1bf4
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2026-02-28 13:10:24 -0500

    Material 3 Theme Builder (Initial/Temporary Theme)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
